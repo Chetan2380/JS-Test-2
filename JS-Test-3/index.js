83d4b300c416ab971dc2be384a9dd8a35f4a353d
Chetan2380 <148186723+Chetan2380@users.noreply.github.com>
2024-05-25 16:14:55 +0530

    Update

---
+++ JS-Test-3/index.js
@@ -0,0 +1,55 @@
+// Q3.
+// array1=[1,2,3,4]
+// array2=[2,4,6,8]
+// array3=[];
+// function intersection(array1,array2){
+//     for(var i=0;i<array1.length;i++){
+//         for(var j=0;j<array1.length;j++){
+//             if(array1[i]===array2[j]){
+//                 array3.push(array1[i])
+//             }
+//         }
+//     }
+//     return array3;
+// }
+// const intersection1=intersection(array1,array2)
+// console.log(intersection1)
+
+
+// Q1.
+// for(var i=1;i<=100;i++){
+//     // console.log(i)
+//     if(i%3===0 && i%5===0){
+//         console.log("Fizzbuzz")
+//     }
+
+//     else if(i%3===0){
+//         console.log("Fizz")
+//     }
+
+//     else if(i%5===0){
+//         console.log("Buzz")
+//     }
+
+//     else{
+//         console.log(i)
+//     }
+// }
+
+// Q5.
+// array=[1,2,2,3,4,4,5]
+// function remdup(){
+//     return array.filter((element,index)=>array.indexOf(element)===index);
+// }
+// var array2=remdup(array)
+// console.log(array2)
+
+// Q2.
+// var str="javascript";
+// function repeat(str){
+//     for(var i=0; i<str.length;i++){
+//         console.log(str[i])
+//     }
+// }
+// repeat(str)
+
